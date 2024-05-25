a40a7634e9f63f589d666a8eb4e9a0e4ed5a3709
Maygodbless3u <105101345+Maygodbless3u@users.noreply.github.com>
2024-05-25 14:05:16 +0000

    datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all js code as newer version
+//alert doesnot work since we are working with nodejs and not any browser
+
+let name = "Aryan";
+let age = 19;
+let isLoggedIn = false;
+
+//int stores upto 2 to the power 53, anymore than that use bigInt
+//null is a stand alone value and also a datatype
+//undefined is when the values are not assigned/defined
+//null is a representation of empty value
+//symbol (mostly used in react), used to find the uniqueness
+//object 
+console.log(typeof null);
+console.log(typeof undefined);
+
+//null is an object but undefined itself is a type
