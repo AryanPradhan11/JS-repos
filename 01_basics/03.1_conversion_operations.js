d06ab7ae75df1c718a9e6f9e969515f4efe66001
Maygodbless3u <105101345+Maygodbless3u@users.noreply.github.com>
2024-05-25 15:21:44 +0000

    checking limitations for coversion

---
+++ 01_basics/03.1_conversion_operations.js
@@ -0,0 +1,15 @@
+// let value = 3;
+// let neg = -value;
+
+// console.log(2%6)
+
+let str1 = "hello ";
+let str2 = "Aryan";
+
+let str3 = str1 + str2;
+//console.log(str3)
+
+// console.log("1" + 2) //output = 12
+// console.log("1" + 2 + 2) //output = 122
+// console.log(2 + 2 + "1") // 41
+console.log(-true)
