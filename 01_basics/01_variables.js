0425ea6d58ed82b1d471443096606a37041f47a9
Maygodbless3u <105101345+Maygodbless3u@users.noreply.github.com>
2024-05-25 13:57:53 +0000

    completed 01

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 12345; //can't change the const to any other value
+let accountEmail = "aryanpradhan773@gmail.com";
+var accountPassword = "12345";
+accountCity = "KTM";
+let accountState;
+
+//accountId = 2;
+accountEmail = "abc.com";
+accountPassword = "ct";
+accountCity = "BTR";
+console.log(accountId);
+
+//console.table([accountEmail, accountId, accountPassword, accountCity]); //provides value in tabular form
+console.log(accountState); 
+/*don't use var. var doesn't recognize scope*/
+
+const a = "Aryan";
+console.log("Hello", a)
