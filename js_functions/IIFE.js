52602c302cbb01535fe20dd9ee21ef03c7e9d5be
Maygodbless3u <105101345+Maygodbless3u@users.noreply.github.com>
2024-06-03 13:20:53 +0000

    iife

--- js_functions/IIFE.js
+++ js_functions/IIFE.js
@@ -1,2 +1,25 @@
 /**Immediately Invoked Function Expression */
 
+function abc() {
+    console.log("Normal functions")
+}
+
+
+
+(function iife() { //named iife
+    // console.log("IIFE functions")
+}) (); //second bracket for execution and first bracket wrapping function into parenthesis
+
+(() => {
+    // console.log("IIFE function for Arrow functions")
+}) ();
+
+//with paranthesis
+((name) => { //parameter is name
+    console.log(`Hello ${name}`)
+})('Aryan'); //Aryan is the argument
+
+(function name(names) {
+    console.log(`Hello ${names}`)
+})('gandu')
+
