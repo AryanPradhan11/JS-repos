d7444aae6db39b763ff2d3ef74d2ef67a3e6b41b
Maygodbless3u <105101345+Maygodbless3u@users.noreply.github.com>
2024-06-03 05:46:01 +0000

    more arrays

---
+++ 01_basics/array2.js
@@ -0,0 +1,29 @@
+const names = ["A", "B", "C", "D"]
+const sur = ["E", "F", "G"]
+// names.push(sur)
+
+// console.log(names)
+
+// let allnames = names.concat(sur)
+// console.log(allnames)
+
+// const spread = [...names, ...sur] //spreading values
+// console.log(spread)
+
+// const anotherarr = [1, 2, 3, [4, 5, 6], 7, [8,9,0]]
+
+// const useableArr = anotherarr.flat(Infinity)
+// console.log(useableArr)
+
+
+
+const array = ["Aryan"]
+
+console.log(Array.isArray("Aryan")) //checks if the array exists
+console.log(Array.from("Aryan"))
+
+
+let score1 = 100
+let score2 = 200
+
+console.log(Array.of(score1, score2)) //stores in array
