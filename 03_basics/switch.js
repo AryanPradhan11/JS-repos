a5fe1a2f7202f5a07df76da91b78c526327d48eb
Maygodbless3u <105101345+Maygodbless3u@users.noreply.github.com>
2024-06-05 14:54:15 +0000

    switch case

---
+++ 03_basics/switch.js
@@ -0,0 +1,18 @@
+//switch
+
+let month = 3
+
+switch (month) {
+    case 1:
+        console.log("Jam")
+        break;
+    case 2:
+        console.log("Feb")
+    case 3: 
+        console.log("Mar")
+    
+    default:
+        break;
+}
+
+//if not break, it will print and run the code below the matched value except default
