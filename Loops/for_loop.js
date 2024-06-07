6a8bc3981c8545359e80aee2c6d765c4df6b168a
Maygodbless3u <105101345+Maygodbless3u@users.noreply.github.com>
2024-06-07 12:03:39 +0000

    for loops

--- Loops/for_loop.js
+++ Loops/for_loop.js
@@ -1,8 +1,14 @@
 //For loops
-const array = [0, 1, 2, 3, 4, 5]
-const element = 0
-for (let i = 0; i < array.length; i++) {
-    element += array[i];
-    return element
+// const array = [0, 1, 2, 3, 4, 5]
+// let element = 0
+// for (let i = 0; i < array.length; i++) {
+//     element += array[i];
+// }
+// console.log(`The sum of the array is ${element}`)
+
+for (let i = 1; i < 10; i++) {
+    console.log(`Outer Loop is ${i}`);
+    for(let j = 1; j<5; j++) {
+        console.log(i + '*' + j + '=' + i*j)
+    }
 }
-console.log(`The sum of the array is ${element}`)
