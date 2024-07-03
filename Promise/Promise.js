a122b2cc1daca0f3d0d0f90c021fc79d3d7904cb
Maygodbless3u <105101345+Maygodbless3u@users.noreply.github.com>
2024-07-03 06:49:45 +0000

    learning promises

--- Promise/Promise.js
+++ Promise/Promise.js
@@ -1,29 +1,19 @@
 //This is a personal practice file for promise
 
 const promise = new Promise(function(resolve, reject) {
-    setTimeout(function() {
-        console.log('Hello Aryan') //if resolve was moved after this line. Continuation at line 17:
-        setTimeout(function() {
-            console.log("You are ready")
-            resolve()
-        }, 1000)
-    } ,1000)
-    
+    setTimeout(function(){
+        console.log('Hello')
+        resolve()
+    }, 1000)
 })
 
-// promise.then(() => {
-//     console.log("Thy promise has been fulfilled")
-// })
-//The promise will be resolved and will print the first console, the .then console and second console
-
-async function getResponse() {
-    try {
-        await promise //first await promise
-        await new Promise(resolve => setTimeout(resolve, 1000)) //wait an additional 1 second
-        console.log('The promise has been fulfilled') //This is console after 1 second
-    } catch (error) {
-        console.log('E: ', error)
-    }
-}
-
-getResponse()
+promise
+.then(function(){
+    console.log('Your promise has been fulfilled')
+})
+.then(function(){
+    console.log('Task 2 accomplished')
+})
+.catch(function() {
+    console.log('Error')
+})
